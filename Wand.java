827f7cd0237bea2b9c221708979192cfcfc29457
ckm45 <ckm45@naver.com>
2023-03-14 18:05:03 +0900

    연습문제 10-2

--- Wand.java
+++ Wand.java
@@ -3,4 +3,20 @@ package com.ckm.game;
 public class Wand {
     private String name; // 지팡이의 이름
     private double power; // 지팡이의 마력
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
 }
--- Wizard.java
+++ Wizard.java
@@ -6,9 +6,33 @@ public class Wizard {
     private String name;
     private Wand wand;
 
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void heal(Hero hero) {
         int basePoint = 10; // 기본회복 포인트
-        int recovPoint = (int) (basePoint * this.wand.power);
+        int recovPoint = (int) (basePoint * wand.getPower());
         hero.setHp(hero.getHp() + recovPoint);
     }
 
