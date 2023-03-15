a6a3f42f09d41fdbf9463f7583803b94b6e35453
ckm45 <ckm45@naver.com>
2023-03-15 13:51:30 +0900

    답 수정

--- 자바 11장/PoisonKinoko.java
+++ 자바 11장/PoisonKinoko.java
@@ -11,7 +11,7 @@ public class PoisonKinoko extends Kinoko {
     public void attack(Hero hero) {
 
         super.attack(hero);
-        if (count != 0) {
+        if (count > 0) {
             System.out.println("추가로, 독 포자를 살포했다!");
             int damage = hero.getHp() / 5;
             hero.setHp(hero.getHp() - damage);
