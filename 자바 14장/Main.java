7e30f3a46aab32fb65d80bd2cb3ccfaf5d05526c
ckm45 <ckm45@naver.com>
2023-03-17 08:53:06 +0900

    자바 연습문제 14-2-1

--- 자바 14장/Main.java
+++ 자바 14장/Main.java
@@ -7,23 +7,10 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        Date now = new Date();
-        System.out.println(now);
-        Calendar calendar = Calendar.getInstance(); // Calendar 추상메소드 여서 new로 못 만든다.
-        calendar.setTime(now);
+        BankAccount a = new BankAccount("4649", 1592);
 
-        int day = calendar.get(Calendar.DATE);
-        System.out.println(day);
+        System.out.println(a);
 
-        calendar.set(Calendar.DATE, day + 100);
-
-        Date future = calendar.getTime();
-
-        SimpleDateFormat format = new SimpleDateFormat("서기 yyyy년 MM월 dd일");
-
-        System.out.println(format.format(future)); // 메소드 format()은 return 타입이 String이다
-
-        // calendar.add(Calendar.DATE,100);
 
     }
 
